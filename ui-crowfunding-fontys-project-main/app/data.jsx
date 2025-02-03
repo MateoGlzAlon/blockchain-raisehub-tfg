1594c43961f60ab7a6b87b8484782adc6f6e6027
MateoGonzalezz <mateo.gonzalez.alonso@gmail.com>
2025-02-03 21:54:52 +0100

    Added repositories from RaiseHub-fontys repository

---
+++ ui-crowfunding-fontys-project-main/app/data.jsx
@@ -0,0 +1,22 @@
+export const DATA = {
+    projectName: "RaiseHub",
+    logoColor: "/logoColor.svg",
+    logoBlack: "/logoBlack.svg",
+    logoWhite: "/logoWhite.svg",
+    heroTitle: "Bring your favourite\nprojects to life",
+    origin: "http://localhost:8080",
+    shortOrigin: "localhost:8080",
+    projectTypes: [
+        "Education",
+        "Environment",
+        "Animal Welfare",
+        "Health",
+        "Human Rights",
+        "Sustainability",
+        "Culture",
+    ],
+    //TO-DO
+    gitHubRepository: "https://github.com/MateoGlzAlon/raisehub-fontys",
+    creatorWeb: "https://glzalomateo.vercel.app",
+    documentation: "https://docs-raisehub.vercel.app",
+}
